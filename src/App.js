906e46809901ab42afd8bc224bbb367b8d827e81
dwinuroktaviani27 <1941720239@student.polinema.ac.id>
2022-05-18 08:43:08 +0700

    React Firebase

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {createContext, useState} from 'react';
+import './Style.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+// import * as firebase from 'firebase';
+import { initializeApp } from 'firebase/app'
+import Header from './Header';
+import routes from './routes';
+import firebaseConfig from './firebase.config'
+export const AuthContext = createContext(null);
+
+export const appInit = initializeApp(firebaseConfig);
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthContext.Provider value={{ isLoggedIn, setIsLoggedIn }}>
+      Is Logged in ? {JSON.stringify(isLoggedIn)}    
+      <div className='App'>
+        <Router>
+          <Header />
+          <Switch>
+            {
+              routes.map(route => (
+                <Route 
+                  key={route.path}
+                  path={route.path}
+                  exact={route.exact}
+                  component={route.main}
+                />
+              ))
+            }
+          </Switch>
+        </Router>
+      </div>
+    </AuthContext.Provider>
   );
 }
 
---
+++ src/firebase.config.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyDNOLekoXJJ3E6ta3fbmVZQVzDUi0Nm7t4",
+    authDomain: "pbf-dnoktaviani2022.firebaseapp.com",
+    projectId: "pbf-dnoktaviani2022",
+    storageBucket: "pbf-dnoktaviani2022.appspot.com",
+    messagingSenderId: "762123229290",
+    appId: "1:762123229290:web:878cb43910fb78736f9213",
+    measurementId: "G-J1LWXQWDLF"
+}
